7dc21bdf0abf2b042462c9b13a7931a64974d642
NANDINEE RAJ <technonr2307@gmail.com>
2026-05-31 12:29:09 +0530

    added maximum subarray

--- WEEK 1/day2/MaxSubarray.java
+++ WEEK 1/day2/MaxSubarray.java
@@ -1,5 +1,19 @@
 package WEEK 1.day2;
 
-public class MaxSubarray {
-    
+class Solution {
+    public int maxSubArray(int[] nums) {
+
+        int currentSum = nums[0];
+        int maxSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+
+            currentSum = Math.max(nums[i],
+                                  currentSum + nums[i]);
+
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
 }
