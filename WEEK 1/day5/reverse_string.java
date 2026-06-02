dcd1eaaa06890cd0a7bff8ff82339e081b48b853
NANDINEE RAJ <technonr2307@gmail.com>
2026-06-02 22:13:23 +0530

    added reverse string

--- WEEK 1/day5/reverse_string.java
+++ WEEK 1/day5/reverse_string.java
@@ -1,5 +1,15 @@
-package WEEK 1.day5;
+class Solution {
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
 
-public class reverse_string {
-    
+        while (left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
 }
