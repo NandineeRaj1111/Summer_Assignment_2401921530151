5c2f14c50cf1e877735650f30c26755291145aca
NANDINEE RAJ <technonr2307@gmail.com>
2026-06-02 22:42:38 +0530

    find anagrams in string

---
+++ WEEK 2/day2/Anagrams_in_string.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+
+        List<Integer> ans = new ArrayList<>();
+
+        if (p.length() > s.length()) return ans;
+
+        int[] pCount = new int[26];
+        int[] window = new int[26];
+
+        for (int i = 0; i < p.length(); i++) {
+            pCount[p.charAt(i) - 'a']++;
+            window[s.charAt(i) - 'a']++;
+        }
+
+        if (Arrays.equals(pCount, window))
+            ans.add(0);
+
+        for (int i = p.length(); i < s.length(); i++) {
+
+            window[s.charAt(i) - 'a']++;
+
+            window[s.charAt(i - p.length()) - 'a']--;
+
+            if (Arrays.equals(pCount, window)) {
+                ans.add(i - p.length() + 1);
+            }
+        }
+
+        return ans;
+    }
+}
