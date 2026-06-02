8f64bf01f0ddb0db3f843fd188c5f10cd6ba3372
NANDINEE RAJ <technonr2307@gmail.com>
2026-06-02 22:04:19 +0530

    added reshape matrix

--- WEEK 1/day4/ReshapeMatrix.java
+++ WEEK 1/day4/ReshapeMatrix.java
@@ -1,5 +1,14 @@
-package WEEK 1.day4;
-
-public class ReshapeMatrix {
-    
+class Solution {
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+        int m=mat.length;
+        int n=mat[0].length;
+        if(m*n!=r*c) return mat;// if reshape possible or not
+        int[][] res = new int[r][c];
+        for(int i=0;i<m*n;i++){//loop till the total elements
+            res[i/c][i%c]=mat[i/n][i%n];//formula for finding row and coloumn
+            //for row= index/column length ;
+            //for col=index % col length;
+        }
+        return res;
+    }
 }
