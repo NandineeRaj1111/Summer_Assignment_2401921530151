b3fd467f2f13e223bc297e089f57554be0f65f8e
NANDINEE RAJ <technonr2307@gmail.com>
2026-06-02 22:14:03 +0530

    added longest common prefix

--- WEEK 1/day5/longest_common_prefix.java
+++ WEEK 1/day5/longest_common_prefix.java
@@ -1,5 +1,13 @@
-package WEEK 1.day5;
-
-public class longest_common_prefix {
-    
+class Solution {
+    public String longestCommonPrefix(String[] s) {
+        if(s==null||s.length==0) return " ";
+        Arrays.sort(s);//sorted string array
+        String first=s[0];//first word
+        String last=s[s.length-1];//last word 
+        int i=0;
+        while(i<first.length() && i<last.length()&& first.charAt(i)==last.charAt(i)){
+            i++;
+        } 
+        return first.substring(0,i);
+        }
 }
