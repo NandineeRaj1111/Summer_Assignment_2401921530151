fc3683284d636344ed13d540622e5a7f10d8dee0
NANDINEE RAJ <technonr2307@gmail.com>
2026-06-10 10:25:05 +0530

    reverse word

---
+++ WEEK 2/day4/decode_string.java
@@ -0,0 +1,37 @@
+class Solution {
+
+    int index = 0;
+
+    public String decodeString(String s) {
+        StringBuilder result = new StringBuilder();
+
+        while (index < s.length() && s.charAt(index) != ']') {
+
+            if (Character.isDigit(s.charAt(index))) {
+
+                int num = 0;
+
+                while (Character.isDigit(s.charAt(index))) {
+                    num = num * 10 + (s.charAt(index) - '0');
+                    index++;
+                }
+
+                index++;
+
+                String decoded = decodeString();
+
+                index++;
+
+                while (num-- > 0) {
+                    result.append(decoded);
+                }
+
+            } else {
+                result.append(s.charAt(index));
+                index++;
+            }
+        }
+
+        return result.toString();
+    }
+}
---
+++ WEEK 2/day5/groupAnagram.java
@@ -0,0 +1,30 @@
+package WEEK 2.day5;
+
+class Solution {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        Map<String, List<String>> map =
+            new HashMap<>();
+
+        for (String s : strs) {
+
+            char[] arr = s.toCharArray();
+
+            Arrays.sort(arr);
+
+            String key = new String(arr);
+
+            map.putIfAbsent(
+                key,
+                new ArrayList<>()
+            );
+
+            map.get(key).add(s);
+        }
+
+        return new ArrayList<>(
+            map.values()
+        );
+    }
+}
