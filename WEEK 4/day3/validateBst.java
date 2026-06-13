f902d3e3c4e101dfefcac633959a642761053b97
NANDINEE RAJ <technonr2307@gmail.com>
2026-06-14 00:29:54 +0530

    validate best

--- WEEK 4/day3/validateBst.java
+++ WEEK 4/day3/validateBst.java
@@ -1,5 +1,37 @@
-package WEEK 4.day3;
+class Solution {
 
-public class validateBst {
-    
+public boolean isValidBST(
+TreeNode root){
+
+return helper(
+root,
+Long.MIN_VALUE,
+Long.MAX_VALUE
+);
+}
+
+boolean helper(
+TreeNode node,
+long min,
+long max){
+
+if(node==null)
+return true;
+
+if(node.val<=min
+|| node.val>=max)
+return false;
+
+return helper(
+node.left,
+min,
+node.val
+)
+&&
+helper(
+node.right,
+node.val,
+max
+);
+}
 }
