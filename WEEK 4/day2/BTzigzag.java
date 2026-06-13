586ccb53145a84bf64c056a0a344c7855f46d9bf
NANDINEE RAJ <technonr2307@gmail.com>
2026-06-14 00:27:18 +0530

    BT zigzag

---
+++ WEEK 4/day2/BTzigzag.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+class Solution {
+
+public List<List<Integer>>
+zigzagLevelOrder(
+TreeNode root){
+
+List<List<Integer>> result=
+new ArrayList<>();
+
+if(root==null)
+return result;
+
+Queue<TreeNode> q=
+new LinkedList<>();
+
+q.offer(root);
+
+boolean left=true;
+
+while(!q.isEmpty()){
+
+int size=q.size();
+
+LinkedList<Integer> level=
+new LinkedList<>();
+
+for(int i=0;i<size;i++){
+
+TreeNode node=q.poll();
+
+if(left)
+level.addLast(node.val);
+else
+level.addFirst(node.val);
+
+if(node.left!=null)
+q.offer(node.left);
+
+if(node.right!=null)
+q.offer(node.right);
+}
+
+result.add(level);
+
+left=!left;
+}
+
+return result;
+}
+}
